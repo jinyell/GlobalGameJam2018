1b3b1079b303efabfb836de94cbd2b41956ee41d
Jinyoung Ko <jinyoung@sglvr.com>
2018-01-26 21:03:33 -0800

    - Added DOTween - Adding Main Menu

---
+++ Global Game Jam/Assets/_Scripts/MenuHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GlobalGameJam
+{
+    public static class MenuHelper
+    {
+        public  static void EnableCanvasGroup(CanvasGroup canvasGroup, bool enable)
+        {
+            canvasGroup.alpha = (enable == true) ? 1f : 0f;
+            canvasGroup.interactable = enable;
+            canvasGroup.blocksRaycasts = enable;
+            canvasGroup.ignoreParentGroups = enable;
+        }
+    }
+}
+
