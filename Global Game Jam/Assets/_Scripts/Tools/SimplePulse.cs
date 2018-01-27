7a1ca79f08d44e7905362f44ccffdaa4c7c878c1
Jinyoung Ko <jinyoung@sglvr.com>
2018-01-26 22:57:47 -0800

    - Added art assets

---
+++ Global Game Jam/Assets/_Scripts/Tools/SimplePulse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GlobalGameJam
+{
+    public class SimplePulse : MonoBehaviour
+    {
+        [SerializeField] private Image pulse;
+        [SerializeField] private Color startColor;
+        [SerializeField] private Color endColor;
+    }
+}
