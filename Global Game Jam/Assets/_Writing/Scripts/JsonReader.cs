91c97aa74540a50bb7ce6b12e4dc2c90b6b09f41
Henry Smith <hgsmith@uci.edu>
2018-01-26 22:32:17 -0800

    Beginning of Json

---
+++ Global Game Jam/Assets/_Writing/Scripts/JsonReader.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader : MonoBehaviour {
+
+    public TextAsset Game;
+    public TextAsset Language;
+
+    static public SceneObj currentScene;
+    static public Dictionary<string, SceneObj> scenes;
+    static public Dictionary<string, string> writing;
+    
+	void Start ()
+    {
+        if(scenes != null)
+            scenes = new Dictionary<string, SceneObj>();
+
+        if (writing != null)
+            writing = new Dictionary<string, string>();
+
+        GameJson game = JsonUtility.FromJson<GameJson>(Game.ToString());
+        parseGame(game);
+        print(game.scenes[1].text);
+    }
+	
+	void parseGame(GameJson game)
+    {
+        foreach(SceneJson scene in game.scenes)
+        {
+            scenes.Add(scene.id, jsonToScene(scene));
+        }
+        currentScene = jsonToScene(game.scenes[0]);
+    }
+
+    private SceneObj jsonToScene(SceneJson scene)
+    {
+        SceneObj obj = new SceneObj();
+
+        obj.conditions = scene.conditions;
+        obj.fallback = scene.fallback;
+        obj.script = scene.script;
+        obj.text = scene.text;
+        obj.options = scene.options;
+
+        return obj;
+    }
+}
+
+/*
+{
+	"scenes":
+	[
+		{
+			"id":"BEGIN",
+			"text":"This is the Start",
+			"options":
+			[
+				{
+					"text":"Yes",
+					"link":"GOOD",
+					"flag":"SAID_YES"
+				},
+				{
+					"text":"No",
+					"link":"BAD"
+				},
+				{
+					"text":"Rude",
+					"link":"BAD",
+					"flag":"RUDE"
+				}
+			]
+		},
+		{
+			"id":"GOOD",
+			"text":"I'm glad you agree!",
+			"options":
+			[
+				{
+					"text":"Continue",
+					"link":"END"
+				}
+			]
+		},
+		{
+			"id":"BAD",
+			"text":"How dare you!",
+			"options":
+			[
+				{
+					"text":"Continue",
+					"link":"END"
+				}
+			]
+		},
+		{
+			"id":"END",
+			"text":"Hold on...",
+			"options":
+			[
+				{
+					"text":"Continue",
+					"link":"END_GOOD"
+				}
+			]
+		},
+		{
+			"id":"END_GOOD",
+			"conditions":["SAID_YES"],
+			"fallback":"END_RUDE",
+			"Script":"TestScript",
+			"text":"You were nice",
+		},
+		{
+			"id":"END_RUDE",
+			"conditions":["RUDE"],
+			"fallback":"END_FINAL",
+			"text":"you were rude",
+		},
+		{
+			"id":"END_FINAL",
+			"text":"I don't care"
+		}
+	]
+}
+
+*/
---
+++ Global Game Jam/Assets/_Writing/Scripts/WritingStructs.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct GameJson
+{
+    public SceneJson[] scenes;
+}
+
+[System.Serializable]
+public struct SceneJson
+{
+    public string id;
+    public string[] conditions;
+    public string script;
+    public string fallback;
+    public string text;
+    public OptionObj[] options;
+}
+
+[System.Serializable]
+public struct SceneObj
+{
+    public string[] conditions;
+    public string text;
+    public string script;
+    public string fallback;
+    public OptionObj[] options;
+}
+
+[System.Serializable]
+public struct OptionObj
+{
+    public string text;
+    public string link;
+    public string flag;
+}
+
+[System.Serializable]
+public struct WritingJson
+{
+    public TextJson[] writing;
+}
+
+[System.Serializable]
+public struct TextJson
+{
+    public string name;
+    public string text;
+}
