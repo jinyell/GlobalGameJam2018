0fd9063929dd2ba0450e10b9c6b8a5629b1d3848
Jinyoung Ko <jinyoung@sglvr.com>
2018-01-26 22:11:10 -0800

    - Added button choices

---
+++ Global Game Jam/Assets/_Scripts/Menu/MenuGUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GlobalGameJam
+{
+    public class MenuGUI : MonoBehaviour
+    {
+        public delegate void OnChoiceSelected(int choiceSelectIndex);
+        public event OnChoiceSelected onChoiceSelected;
+
+        [SerializeField] private GameObject buttonPrefab;
+        [SerializeField] private GameObject choicesContainer;
+        [SerializeField] private CanvasGroup choicesCanvasGroup;
+
+        public void SelectButton(GameObject buttonSelect)
+        {
+            MenuHelper.EnableCanvasGroup(choicesCanvasGroup, false);
+
+            if(onChoiceSelected != null)
+            {
+                int selectIndex = System.Int32.Parse(buttonSelect.name);
+                onChoiceSelected(selectIndex);
+            }
+        }
+
+        public void CreateChoices(string[] dialogueChoices)
+        {
+            for (int i = 0; i < dialogueChoices.Length; i++)
+            {
+                GameObject choiceButton = (GameObject)GameObject.Instantiate(buttonPrefab) as GameObject;
+                Text choice = choiceButton.GetComponentInChildren<Text>();
+                choiceButton.name = i.ToString();
+                choice.text = dialogueChoices[i];
+                choiceButton.transform.SetParent(choicesContainer.transform);
+                choiceButton.transform.localScale = Vector3.one;
+                choiceButton.transform.localPosition = new Vector3(choiceButton.transform.localPosition.x, choiceButton.transform.localPosition.y, 0f);
+                choiceButton.SetActive(true);
+            }
+            MenuHelper.EnableCanvasGroup(choicesCanvasGroup, true);
+        }
+
+        private void Start()
+        {
+            string[] createChoices = new string[2] { "What time are you thinking about going?", "Who else is going?" };
+            CreateChoices(createChoices);
+        }
+    }
+}
---
+++ Global Game Jam/Assets/_Scripts/Test/ChoiceReceived.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGameJam
+{
+    public class ChoiceReceived : MonoBehaviour
+    {
+        [SerializeField] private MenuGUI menuGUI;
+
+        public void ChoiceSelected(int choiceIndex)
+        {
+            Debug.Log("The choice index chosen is: " + choiceIndex);
+        }
+
+        private void OnEnable()
+        {
+            menuGUI.onChoiceSelected += ChoiceSelected;
+        }
+
+        private void OnDisable()
+        {
+            menuGUI.onChoiceSelected -= ChoiceSelected;
+        }
+    }
+}
