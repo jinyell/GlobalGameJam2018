f6e37c6c76628f8ce76879272ac03922d329faa8
Jinyoung Ko <jinyoung@sglvr.com>
2018-01-27 18:09:08 -0800

    - Added COMPLETED Day 01 JSON file - Added folders/json file for other days - Added new art assets - Added audio

---
+++ Global Game Jam/Assets/_Scripts/Menu/GameMenusContainer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGameJam
+{
+    public class GameMenusContainer : MonoBehaviour
+    {
+        [System.Serializable]
+        public class GameMenu
+        {
+            public enum GameScene
+            {
+                CLASSROOM,
+                DESK,
+                BEDROOM,
+                CAFETERIA,
+                PHONE,
+                HALLWAY
+            }
+
+            public GameScene gameScene;
+            public CanvasGroup canvasGroup;
+        }
+
+        [SerializeField] private CanvasGroup gamesMenuContainer;
+        [SerializeField] private GameMenu[] gameMenus;
+        [SerializeField] private GameMenu.GameScene startMenu;
+
+        private GameMenu.GameScene currentScene;
+
+        public void SelectMenu(GameMenu.GameScene gameScene)
+        {
+            ShowSelectedMenu(gameScene);
+        }
+
+        private void Start()
+        {
+            MenuHelper.EnableCanvasGroup(gamesMenuContainer, true);
+            ShowSelectedMenu(startMenu);
+        }
+
+        private void ShowSelectedMenu(GameMenu.GameScene selectedMenu)
+        {
+            for (int i = 0; i < gameMenus.Length; i++)
+            {
+                MenuHelper.EnableCanvasGroup(gameMenus[i].canvasGroup, false);
+            }
+            currentScene = selectedMenu;
+            MenuHelper.EnableCanvasGroup(gameMenus[(int)currentScene].canvasGroup, true);
+        }
+    }
+}
