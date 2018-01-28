a3a2c7102d1cab3cbceae4b284d844e5c0024f96
Jinyoung Ko <jinyoung@sglvr.com>
2018-01-28 11:08:32 -0800

    - Added audio functionality

---
+++ Global Game Jam/Assets/_Scripts/Tools/AudioHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGameJam
+{
+    [RequireComponent (typeof(AudioSource))]
+    public class AudioHelper : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] music;
+
+        private AudioSource audioSource;
+
+        public void PlayGamePlayMusic()
+        {
+            audioSource.loop = false;
+            audioSource.Stop();
+            PlayRandomNextSong();
+        }
+
+        private void Awake()
+        {
+            audioSource = this.GetComponent<AudioSource>();
+        }
+
+        private void Start()
+        {
+            PlayIntroSong();
+        }
+
+        private void PlayIntroSong()
+        {
+            audioSource.clip = music[0];
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+        private void PlayRandomNextSong()
+        {
+            audioSource.clip = music[Random.Range(1, music.Length)];
+            audioSource.Play();
+            Invoke("PlayNextSong", audioSource.clip.length);
+        }
+    }
+}
